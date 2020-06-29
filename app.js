e0f9e60d7b273e6b60ad122c0f00b0af4e2e3e56
marcelm <mpernia@riservi.com>
2020-06-29 01:38:21 -0500

    Carga de proyecto

---
+++ app.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('#form');
+const input = document.querySelector('#input');
+const text = document.querySelector('#text');
+
+form.onsubmit = function(e) {
+  e.preventDefault();
+  text.innerHTML = input.value;
+}
